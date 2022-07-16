a67908eb99a9798b4b8ee74958881b5be66fac59
nahuelmangano <mangano.nahuel@gmail.com>
2022-07-16 01:04:48 -0300

    Primera Subida front_end

---
+++ src/app/components/arg-prog-logo/arg-prog-logo.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ArgProgLogoComponent } from './arg-prog-logo.component';
+
+describe('ArgProgLogoComponent', () => {
+  let component: ArgProgLogoComponent;
+  let fixture: ComponentFixture<ArgProgLogoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ArgProgLogoComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ArgProgLogoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
